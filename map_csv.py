0871f4f8da9e28fb22252f69b9f572f315190ee4
Han Han <hhan@redhat.com>
2019-07-03 10:38:58 +0800

    Finish QMP to libvirt API mapping part
    Signed-off-by: Han Han <hhan@redhat.com>

--- map_csv.py
+++ map_csv.py
@@ -1,87 +1,92 @@
 #!/usr/bin/python3
-import subprocess
+import subprocess as sp
 import argparse
 import os
-import argparse
+import sys
 import csv
-from functools import reduce
+import tempfile
 
 
-def get_apis_from_src(src_dir):
-    cmd = 'grep "virDomain[a-zA-Z0-9]*" %s -o|sed "s/^vir/qemu/"' % os.path.join(
-        src_dir, 'libvirt_public.syms')
-    return set(subprocess.getoutput(cmd).split('\n'))
+class LibvirtQemu:
+    def __init__(self, src, makecmd):
+        self.src = src
+        self.makecmd = makecmd
+        self.tmpdir = tempfile.mkdtemp()
+        self.libvirt_apis = self.get_libvirt_apis()
+        self.qemu_apis = {i.replace("vir", "qemu", 1)
+                          for i in self.libvirt_apis}
+        self.qemu_cscope = self.gen_qemu_cscope()
+        self.virsh_cscope = self.gen_virsh_cscope()
+        self.mon_funcs = self.get_mon_funcs()
 
+    def get_libvirt_apis(self):
+        cmd = 'grep "virDomain[a-zA-Z0-9]*" %s -o' % os.path.join(
+            self.src, 'src/libvirt_public.syms')
+        return set(sp.check_output(cmd, text=True, shell=True).split('\n'))
 
-def gen_cscope(src_dir):
-    os.system(
-        "find %s -name *.c -o -name *.h > cscope.files" % os.path.join(src_dir, 'qemu'))
-    os.system('cscope -b -q -k')
+    def gen_cscope(self, subdir, name):
+        src = os.path.join(self.src, subdir)
+        cscope = os.path.join(self.tmpdir, '%s.out' %name)
+        cmd = 'cscope -b -f %s -s %s -R' % (cscope, src)
+        try:
+            sp.check_output(cmd, text=True, shell=True)
+        except sp.CalledProcessError as e:
+            sys.exit("%s cscope generate failed: %s\nreturn: %d" %
+                     (name, cmd, e.returncode))
+        return cscope
 
+    def gen_qemu_cscope(self):
+        return self.gen_cscope('src/qemu', 'qemu')
 
-def get_mon_functions(makecmd_wrapper):
-    cmd = "cscope -d -L3 %s |grep -v '^[a-zA-Z_0-9]*\.h' | grep -v ATTRIBUTE_ | awk '{print $2}'" % (
-        makecmd_wrapper)
-    #print("get_mon_functions cmd: %s" % cmd)
-    return set(subprocess.getoutput(cmd).split('\n'))
+    def gen_virsh_cscope(self):
+        return self.gen_cscope('tools', 'virsh')
 
+    def get_mon_funcs(self):
+        cmd = "cscope -d -L3 %s|grep -v '^[a-zA-Z_0-9]*\.h'|grep -v ATTRIBUTE_ |awk '{print $2}'" % (
+            self.makecmd)
+        return set(sp.check_output(cmd, text=True, shell=True).split('\n'))
 
-def get_qmp(mon_wrapper, makecmd_wrapper, src_dir):
-    qmp_mon_src = os.path.join(src_dir, 'qemu', 'qemu_monitor_json.c')
-    cmd = """awk '/%s/,/^}/' %s|awk '/%s/,/)/'|grep '\"[a-zA-Z:_-]*\"' -o |tr '\n' ','""" % (
-        mon_wrapper, qmp_mon_src, makecmd_wrapper)
-    return subprocess.getoutput(cmd)
+    def get_qmp(self, mon_func):
+        qmp_mon_src = os.path.join(self.src, 'src/qemu/qemu_monitor_json.c')
+        cmd = """awk '/%s/,/^}/' %s|awk '/%s/,/)/'|grep '"[a-zA-Z:_-]*"' -o |tr '\n' ','""" % (
+            mon_func, qmp_mon_src, self.makecmd)
+        return sp.check_output(cmd, text=True, shell=True)
 
+    def get_callers(self, fn, cscope):
+        cmd = "cscope -d -L3 %s -f %s |grep -v '^[a-zA-Z_0-9]*\.h' | grep -v ATTRIBUTE_ | awk '{print $2}'" % (
+            fn, cscope)
+        return {x for x in set(sp.check_output(cmd, text=True, shell=True).split('\n')) if x != ''}
 
-def get_parent_callers(fn):
-    cmd = "cscope -d -L3 %s |grep -v '^[a-zA-Z_0-9]*\.h' | grep -v ATTRIBUTE_ | awk '{print $2}'" % (
-        fn)
-    callers = {x for x in set(subprocess.getoutput(cmd).split('\n')) if x != ''}
-    if not callers or callers == {fn}:
-        return {fn}
-    return callers
+    def get_top_callers(self, fn, cscope):
+        stack = []
+        top_callers = set()
+        accessed = set()
+        func = fn
+        stack.append(func)
+        while stack != []:
+            func = stack.pop()
+            if func not in accessed:
+                accessed.add(func)
+                callers = self.get_callers(func, cscope)
+                if func in callers or callers == set():
+                    top_callers.add(func)
+                for caller in callers:
+                    stack.append(caller)
+        return top_callers
 
+    def writecsv(self, csv_path):
+        with open(csv_path, 'w') as csvfile:
+            header = ["Monitor Wrapper", "QMP", "API callers", "Callers not in APIs"]
+            writer = csv.DictWriter(csvfile, fieldnames=header, delimiter='|')
+            writer.writeheader()
 
-def get_top_callers(fns):
-    callers = fns
-    while True:
-        next_callers = set(reduce(lambda x, y: x | y, map(
-            lambda x: get_parent_callers(x), callers)))
-        if callers == next_callers:
-            return callers
-        callers = next_callers
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--source', help='The source directory of libvirt',
-                        required=True, type=str)
-    parser.add_argument(
-        '-o', '--output', help='The output csv file', default='out.csv', type=str)
-    args = parser.parse_args()
-
-    makecmd_wrapper = 'qemuMonitorJSONMakeCommand'
-    src_dir = args.source
-
-    gen_cscope(src_dir)
-    mon_wrappers = get_mon_functions(makecmd_wrapper)
-
-    with open(args.output, 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=[
-                                "Monitor Wrapper", "QMP", "Qemu driver caller", "Callers not in qemu drivers"], delimiter='|')
-        writer.writeheader()
-
-        for mon_wrapper in mon_wrappers:
-            print("Processing mon wrapper: %s" % mon_wrapper)
-            qmp = get_qmp(mon_wrapper, makecmd_wrapper, src_dir)
-            apis = get_apis_from_src(src_dir)
-            mon_callers = get_top_callers({mon_wrapper})
-            mon_callers_inapi_str = '\n'.join(
-                [caller for caller in mon_callers if caller in apis])
-            mon_callers_notinapi_str = '\n'.join(
-                [caller for caller in mon_callers if caller not in apis])
-            writer.writerow({
-                "Monitor Wrapper": mon_wrapper,
-                "QMP": qmp,
-                "Qemu driver caller": mon_callers_inapi_str,
-                "Callers not in qemu drivers": mon_callers_notinapi_str})
+            for mon in self.mon_funcs:
+                qmp = self.get_qmp(mon)
+                top_callers = self.get_top_callers(mon, self.qemu_cscope)
+                callers_api = "\n".join([j.replace('qemu', 'vir', 1) for j in {i for i in top_callers if i in self.qemu_apis}])
+                callers_not_api = "\n".join([i for i in top_callers if i not in self.qemu_apis])
+                writer.writerow({
+                    header[0]: mon,
+                    header[1]: qmp,
+                    header[2]: callers_api,
+                    header[3]: callers_not_api})
