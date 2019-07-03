938a747a15e6a52765a777b338882c9c62e9d5f0
Han Han <hhan@redhat.com>
2019-07-03 14:53:30 +0800

    Add command line arguments
    Signed-off-by: Han Han <hhan@redhat.com>

--- map_csv.py
+++ map_csv.py
@@ -7,55 +7,61 @@ import csv
 import tempfile
 
 
+def rm_empty_str(objs):
+    return {i for i in objs if i != ''}
+
 class LibvirtQemu:
-    def __init__(self, src, makecmd):
+    def __init__(self, src, mode):
+        if mode not in {'qmp', 'ga'}:
+            raise TypeError('mode should be "qmp" or "ga"')
         self.src = src
-        self.makecmd = makecmd
+        self.mode = mode
+        if mode == 'qmp':
+            self.makecmd = 'qemuMonitorJSONMakeCommand'
+        if mode == 'ga':
+            self.makecmd = 'qemuAgentMakeCommand'
         self.tmpdir = tempfile.mkdtemp()
+        self.qemu_cscope_source = os.path.join(self.src, 'src/qemu')
+        self.qemu_cscope = self.gen_qemu_cscope('qemu')
         self.libvirt_apis = self.get_libvirt_apis()
         self.qemu_apis = {i.replace("vir", "qemu", 1)
                           for i in self.libvirt_apis}
-        self.qemu_cscope = self.gen_qemu_cscope()
-        self.virsh_cscope = self.gen_virsh_cscope()
         self.mon_funcs = self.get_mon_funcs()
 
     def get_libvirt_apis(self):
         cmd = 'grep "virDomain[a-zA-Z0-9]*" %s -o' % os.path.join(
             self.src, 'src/libvirt_public.syms')
-        return set(sp.check_output(cmd, text=True, shell=True).split('\n'))
+        return rm_empty_str(sp.check_output(cmd, shell=True, text=True).split('\n'))
 
-    def gen_cscope(self, subdir, name):
-        src = os.path.join(self.src, subdir)
+    def gen_qemu_cscope(self, name):
         cscope = os.path.join(self.tmpdir, '%s.out' %name)
-        cmd = 'cscope -b -f %s -s %s -R' % (cscope, src)
+        cmd = 'cscope -b -f %s -s %s -R' % (cscope, self.qemu_cscope_source)
         try:
-            sp.check_output(cmd, text=True, shell=True)
+            sp.check_output(cmd, shell=True)
         except sp.CalledProcessError as e:
             sys.exit("%s cscope generate failed: %s\nreturn: %d" %
                      (name, cmd, e.returncode))
         return cscope
 
-    def gen_qemu_cscope(self):
-        return self.gen_cscope('src/qemu', 'qemu')
-
-    def gen_virsh_cscope(self):
-        return self.gen_cscope('tools', 'virsh')
-
     def get_mon_funcs(self):
-        cmd = "cscope -d -L3 %s|grep -v '^[a-zA-Z_0-9]*\.h'|grep -v ATTRIBUTE_ |awk '{print $2}'" % (
-            self.makecmd)
-        return set(sp.check_output(cmd, text=True, shell=True).split('\n'))
+        cmd = "cscope -d -L3 %s -s %s -f %s|grep -v '^[a-zA-Z_0-9]*\.h'|grep -v ATTRIBUTE_ |awk '{print $2}'" % (
+            self.makecmd, self.qemu_cscope_source, self.qemu_cscope)
+        return rm_empty_str(sp.check_output(cmd, shell=True, text=True).split('\n'))
 
-    def get_qmp(self, mon_func):
-        qmp_mon_src = os.path.join(self.src, 'src/qemu/qemu_monitor_json.c')
+    def get_qemu_exec(self, mon_func):
+        if self.mode == 'qmp':
+            mon_file = 'qemu_monitor_json.c'
+        if self.mode == 'ga':
+            mon_file = 'qemu_agent.c'
+        qemu_mon_src = os.path.join(self.src, 'src/qemu/%s' %mon_file)
         cmd = """awk '/%s/,/^}/' %s|awk '/%s/,/)/'|grep '"[a-zA-Z:_-]*"' -o |tr '\n' ','""" % (
-            mon_func, qmp_mon_src, self.makecmd)
-        return sp.check_output(cmd, text=True, shell=True)
+            mon_func, qemu_mon_src, self.makecmd)
+        return sp.check_output(cmd, shell=True, text=True)
 
-    def get_callers(self, fn, cscope):
-        cmd = "cscope -d -L3 %s -f %s |grep -v '^[a-zA-Z_0-9]*\.h' | grep -v ATTRIBUTE_ | awk '{print $2}'" % (
-            fn, cscope)
-        return {x for x in set(sp.check_output(cmd, text=True, shell=True).split('\n')) if x != ''}
+    def get_callers(self, fn):
+        cmd = "cscope -d -L3 %s -f %s -s %s|grep -v '^[a-zA-Z_0-9]*\.h' | grep -v ATTRIBUTE_ | awk '{print $2}'" % (
+            fn, self.qemu_cscope, self.qemu_cscope_source)
+        return rm_empty_str(sp.check_output(cmd, shell=True, text=True).split('\n'))
 
     def get_top_callers(self, fn, cscope):
         stack = []
@@ -67,7 +73,7 @@ class LibvirtQemu:
             func = stack.pop()
             if func not in accessed:
                 accessed.add(func)
-                callers = self.get_callers(func, cscope)
+                callers = self.get_callers(func)
                 if func in callers or callers == set():
                     top_callers.add(func)
                 for caller in callers:
@@ -81,12 +87,21 @@ class LibvirtQemu:
             writer.writeheader()
 
             for mon in self.mon_funcs:
-                qmp = self.get_qmp(mon)
+                qemu_exec = self.get_qemu_exec(mon)
                 top_callers = self.get_top_callers(mon, self.qemu_cscope)
                 callers_api = "\n".join([j.replace('qemu', 'vir', 1) for j in {i for i in top_callers if i in self.qemu_apis}])
                 callers_not_api = "\n".join([i for i in top_callers if i not in self.qemu_apis])
                 writer.writerow({
                     header[0]: mon,
-                    header[1]: qmp,
+                    header[1]: qemu_exec,
                     header[2]: callers_api,
                     header[3]: callers_not_api})
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--source', help="The libvirt source directory", required=True)
+    parser.add_argument('-m', '--mode', help="The mode of API mappings.'qmp': The qemu qmp to libvirt API mappings; 'ga': The qemu guest agent command to libvirt API mappings", required=True)
+    parser.add_argument('-o', '--output', help="The csv output file", default='output.csv')
+    args = parser.parse_args()
+
+    LibvirtQemu(args.source, args.mode).writecsv(args.output)
